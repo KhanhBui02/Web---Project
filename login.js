dcc3e48f05d52ec9f52a8828af107e98834a204a
KhanhBui02 <buidoanbuukhanh0123456@gmail.com>
2021-12-21 18:33:57 +0700

    6:33pm 21/12/2021

--- login.js
+++ login.js
@@ -1,17 +1,27 @@
 function validate() {
     var u = document.getElementById("username").value;
     var p = document.getElementById("password").value;
-     
-    if(u=="") {
-    alert("Please enter Email");
-    return false;
+    var id = "thanhloi@gmail.com";
+    var ps = "123";
+    if(u == "" || p=="")
+    {
+        if(u=="") {
+            alert("Please enter Email");
+            return false;
+            }
+            if(p =="") {
+            alert("Please enter Password");
+            return false;
+            }
     }
-    if(p =="") {
-    alert("Please enter Password");
-    return false;
-    }
-    alert("Sign in success");
-    return true;
+    else{
+        if(u != id || p != ps )
+        {
+            alert("Incorrect Email or Password");
+            return false;
+        } 
+        return true;
+    } 
 }
 
 function validatesignup(){
@@ -26,5 +36,31 @@ function validatesignup(){
     }
 
     alert("Sign up success");
+    return true;
+}
+
+function showbill(){
+    var f = document.getElementById("fill");
+    if(f.style.display === "none"){
+        f.style.display = "flex"
+    }
+    else{
+        f.style.display = "none"
+    }
+}
+
+function confirm() {
+    var e = document.getElementById("email").value;
+    var f = document.getElementById("f_name").value;
+    var l = document.getElementById("l_name").value;
+    var com = document.getElementById("company").value;
+    var t = document.getElementById("town").value;
+    var s = document.getElementById("street").value;
+    var p = document.getElementById("phone").value;
+    if(e=="" || f=="" ||l=="" ||com=="" ||t=="" ||s==""||p=="") {
+        alert("Please enter full information");
+        return false;
+    }
+    alert("Payment success");
     return true;
 }
